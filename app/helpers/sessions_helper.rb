17cfb1e4599ad1e8c0775d2a42fb6d5ca5070bda
suzuki <s7.91yull@gmail.com>
2019-06-30 03:51:03 +0000

    made advanced-login

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -5,18 +5,48 @@ module SessionsHelper
   end
   
   def current_user
-    if session[:user_id]
-      @current_user ||= User.find_by(id: session[:user_id])
+    #session[:user_id]をuser_idに代入した結果user_idが存在すれば下記の処理を実行
+    if (user_id = session[:user_id])
+      @current_user ||= User.find_by(id: user_id)
+    #cookiesに保存されているuser_idがある場合は以下の処理を実行
+    elsif (user_id = cookies.signed[:user_id])
+      user = User.find_by(id: user_id)
+      #user.rb内のauthenticated?メソッドを実行し、cookiesのrememberトークンがrememberダイジェストと一致するか確認
+      if user && user.authenticated?(cookies[:remember_token])
+        log_in user
+        @current_user = user
+      end
     end
   end
+  
   
   def logged_in?
     !current_user.nil?
   end
   
+  #永続的なセッションを消去する
+  def forget(user)
+    #user.rb内のforgetメソッドを実行し、userのremember_digestをnilにする
+    user.forget
+    cookies.delete(:user_id)
+    cookies.delete(:remember_token)
+  end
+  
+
+  
   def log_out
+    #現在のユーザーの永続セッションを消去する(ログアウトすると永続セッションが自動的に解除される)
+    forget(current_user)
     session.delete(:user_id)
     @current_user = nil
+  end
+  
+  def remember(user)
+    #引数を用いてuser.rb内のrememberメソッドを呼び出す
+    user.remember
+    #ユーザーidとrememberトークンを永続セッションにする
+    cookies.permanent.signed[:user_id] = user.id
+    cookies.permanent[:remember_token] = user.remember_token
   end
   
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,40 @@
 class User < ApplicationRecord
+  attr_accessor :remember_token
   before_save{self.email = email.downcase}
   validates :name,presence: true,length: {maximum: 50}
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
   validates :email,presence: true, length: { maximum: 255 }, format: {with: VALID_EMAIL_REGEX},uniqueness: {case_sensitive: false }
   has_secure_password
   validates :password,presence: true,length: {minimum: 6}
+  
+  #引数で渡されたものをハッシュ化する
+  def User.digest(string)
+    cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST :
+                                                  BCrypt::Engine.cost
+    BCrypt::Password.create(string, cost: cost)
+  end
+
+  #新しいトークンを作成
+  def User.new_token
+    SecureRandom.urlsafe_base64
+  end
+  
+  def remember
+    #新しく作成したトークンをその人のrememberトークンとして設定
+    self.remember_token = User.new_token
+    #作成したremember_tokenをハッシュ化し、remeber_digestに保存する
+    update_attribute(:remember_digest,User.digest(remember_token))
+  end
+  
+  def authenticated?(remember_token)
+    return false if remember_digest.nil?
+    BCrypt::Password.new(remember_digest).is_password?(remember_token)
+  end
+  
+  
+  def forget
+    update_attribute(:remember_digest,nil)
+  end 
+  
 end
 
