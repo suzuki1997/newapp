df568c693c32de3fcbb70dcaa268f29f4d519d80
suzuki <s7.91yull@gmail.com>
2019-07-30 09:35:49 +0000

    change routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,25 +2,36 @@ Rails.application.routes.draw do
 
   root "top#index"
   get "/contact",to:"top#contact"
-  get '/signup',to:"users#new"
-  get "/login",to:"sessions#new"
-  post "/login",to:"sessions#create"
-  delete "/logout",to:"sessions#destroy"
-  resources :users do
-    member do
-      get :following,:followers
+  
+  controller :users do
+    resources :users
+    resources :users do
+      member do
+        get :following,:followers
+      end
     end
+    get "users/:id/likes",to:"users#likes"
+    get '/signup',to:"users#new"
   end
-  get "users/:id/likes",to:"users#likes"
   
-  get "/posts/:id",to:"posts#show"
+  controller :sessions do
+    get "/login",to:"sessions#new"
+    post "/login",to:"sessions#create"
+    delete "/logout",to:"sessions#destroy"
+  end
+
+  controller :posts do
+    resources :posts,only:[:create,:destroy]
+    get "/posts/:id",to:"posts#show"
+    get "posts/index",to:"posts#index"
+  end
   
-  post "likes/:post_id/create",to: "likes#create"
-  post "likes/:post_id/destroy",to: "likes#destroy"
+  controller :likes do
+    post "likes/:post_id/create",to: "likes#create"
+    post "likes/:post_id/destroy",to: "likes#destroy"
+  end
   
-  
-  resources :posts,only:[:create,:destroy]
   resources :relationships,only:[:create,:destroy]
   
-  get "posts/index",to:"posts#index"
+  
 end
